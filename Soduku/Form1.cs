396a9cb05d4d18cf06b41ae744bd6cdbc6666abd
ddabb <417144452@qq.com>
2018-12-07 23:50:06 +0800

    反射

--- Soduku/Form1.cs
+++ Soduku/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Soduku
@@ -21,7 +15,33 @@ namespace Soduku
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.postion_8_8.Text = "9";
+            int UPPER = 9;
+            for (int i = 0; i < UPPER; i++)
+            {
+                for (int j = 0; j < UPPER; j++)
+                {
+                    object obj = GetType().GetField("postion_"+i+"_"+j,
+                            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance
+                                                                     | System.Reflection.BindingFlags.IgnoreCase)
+                        ?.GetValue(this);
+                    TextBox testBox = (TextBox)obj;
+                    testBox.Text = "" +(1+(i+j)%9);
+                    testBox.Dock = DockStyle.Fill;
+
+                    testBox.Margin = new Padding(0);
+                    testBox.Multiline = true;
+
+                    testBox.Size = new Size(50, 50);
+                    testBox.TabIndex = 0;
+                    testBox.TextAlign = HorizontalAlignment.Center;
+                    testBox.Font = new Font("楷体", 40F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(134)));
+                }
+            }
+      
+
+
+
+
         }
     }
 }
