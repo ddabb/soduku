dee0bf95baec7b202aa7a88cce709c49794a5e9b
ddabb <417144452@qq.com>
2018-12-16 14:30:56 +0800

    提交测试类

--- SodukuTest/Program.cs
+++ SodukuTest/Program.cs
@@ -1,15 +1,210 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace SodukuTest
 {
     class Program
     {
+        //出题，采用静态数组
+
+        static int[,] pu = new int[9, 9]
+
+ {
+
+ {0,0,0,7,2,8,0,0,0},
+
+ {0,9,0,0,5,1,6,0,0},
+
+ {0,0,0,0,6,0,0,8,2},
+
+ {3,0,0,8,0,2,7,0,4},
+
+ {1,7,4,0,3,0,0,2,0},
+
+ {2,8,0,5,0,0,0,3,0},
+
+ {0,1,0,3,0,0,2,0,0},
+
+ {0,0,7,0,4,6,0,0,5},
+
+ {0,0,6,1,0,0,0,4,9} };
+
+        static string result = "";
+
         static void Main(string[] args)
+
         {
+
+            Show();
+
+            GetAnswer(0);
+
+            Console.ReadLine();
+
         }
+
+        /// <summary>
+
+        /// 验证函数
+
+        /// </summary>
+
+        /// <param name="i"></param>
+
+        /// <param name="j"></param>
+
+        /// <returns></returns>
+
+        static bool IsValid(int i, int j)
+
+        {
+
+            int n = pu[i, j];
+
+            int[] query = new int[9] { 0, 0, 0, 3, 3, 3, 6, 6, 6 };
+
+
+
+            int t, u;
+
+            //每一行每一列是否重复
+
+            for (t = 0; t < 9; t++)
+
+            {
+
+                if ((t != i && pu[t, j] == n) || (t != j && pu[i, t] == n))
+
+                    return false;
+
+            }
+
+            //每个九宫格是否重复
+
+            for (t = query[i]; t < query[i] + 3; t++)
+
+            {
+
+                for (u = query[j]; u < query[j] + 3; u++)
+
+                {
+
+                    if ((t != i || u != j) && pu[t, u] == n)
+
+                        return false;
+
+                }
+
+            }
+
+            return true;
+
+
+
+        }
+
+        /// <summary>
+
+        /// 显示函数
+
+        /// </summary>
+
+        static void Show()
+
+        {
+            result = "";
+            for (var i = 0; i < 9; i++)
+
+            {
+
+                for (var j = 0; j < 9; j++)
+
+                {
+                    result +=pu[i, j];
+                    Console.Write(pu[i, j] + " ");
+
+                
+
+                }
+
+                Console.WriteLine();
+
+            }
+          
+            Console.WriteLine("----------------------------------------------");
+
+            Console.WriteLine(result);
+
+        }
+
+
+
+        /// <summary>
+
+        /// 使用回溯算法求解
+
+        /// </summary>
+
+        /// <param name="n"></param>
+
+        static void GetAnswer(int n)
+
+        {
+
+            if (n == 81)
+
+            {//是否已经是最后一个格子
+
+                Show();
+
+                return;
+
+            }
+
+
+
+            int i = n / 9, j = n % 9;
+
+
+
+            if (pu[i, j] != 0)
+
+            {//如果当前格子不需要填数字，就跳到下一个格子
+
+                GetAnswer(n + 1);
+
+                return;
+
+            }
+
+
+
+            for (int k = 0; k < 9; k++)
+
+            {
+
+                pu[i, j]++;//当前格子进行尝试所有解
+
+                if (IsValid(i, j))
+
+                    GetAnswer(n + 1);//验证通过，就继续下一个
+
+            }
+
+
+
+            pu[i, j] = 0;  //如果上面的单元无解，还原，就回溯
+
+            return;
+
+        }
+
+ 
+
     }
 }
