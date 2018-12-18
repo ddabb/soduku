6b89360364cf99871ccdbd5eef45a742b9561660
ddabb <417144452@qq.com>
2018-12-18 08:21:47 +0800

    先找最小初盘 再配置一件提示数的功能

--- SodukuQuestion/SodukuQuestion.cs
+++ SodukuQuestion/SodukuQuestion.cs
@@ -94,7 +94,7 @@ namespace SodukuFactory
                         }
                     }
                 }
-            } while (!IsVaildQuestion(tempquestion));
+            } while (!new SodukuValid(tempquestion).IsVaildQuestion());
 
             return tempquestion;
         }
@@ -120,140 +120,13 @@ namespace SodukuFactory
         }
 
 
-        public static bool IsVaildQuestion(List<List<int>> param)
-        {
-            List<List<int>> pu = JsonConvert.DeserializeObject<List<List<int>>>(JsonConvert.SerializeObject(param));
-            _resultCount = 0;
-            GetAnswer(0, pu);
-            return _resultCount == 1;
-        }
 
 
-        /// <summary>
-        /// 验证函数
-        /// </summary>
-        /// <param name="i"></param>
-        /// <param name="j"></param>
-        /// <returns></returns>
-        static bool IsValid(int i, int j, List<List<int>> pu)
-
-        {
-            int n = pu[i][j];
-
-            int[] query = new int[9] {0, 0, 0, 3, 3, 3, 6, 6, 6};
 
 
-            int t, u;
-
-            //每一行每一列是否重复
-
-            for (t = 0; t < 9; t++)
-
-            {
-                if (t != i && pu[t][j] == n || t != j && pu[i][t] == n)
-
-                    return false;
-            }
-
-            //每个九宫格是否重复
-
-            for (t = query[i]; t < query[i] + 3; t++)
-
-            {
-                for (u = query[j]; u < query[j] + 3; u++)
-
-                {
-                    if ((t != i || u != j) && pu[t][u] == n)
-
-                        return false;
-                }
-            }
-
-            return true;
-        }
-
-        static string _result = "";
-        private static bool AllValue = false;
-
-        private static int _resultCount = 0;
-
-        /// <summary>
-        /// 显示函数
-        /// </summary>
-        static void GetResult(List<List<int>> pu)
-
-        {
-            _result = "";
-            for (var i = 0; i < 9; i++)
-
-            {
-                for (var j = 0; j < 9; j++)
-
-                {
-                    _result += pu[i][j];
-                }
-            }
+  
 
 
-            _resultCount += 1;
-        }
 
-        /// <summary>
-        /// 使用回溯算法求解
-        /// </summary>
-        /// <param name="n"></param>
-        static void GetAnswer(int n, List<List<int>> pu)
-
-        {
-            if (n == 81)
-
-            {
-                //是否已经是最后一个格子
-
-                GetResult(pu);
-
-                return;
-            }
-
-            if (!AllValue)
-            {
-                if (_resultCount > 1)
-                {
-                    return;
-                }
-            }
-
-            int i = n / 9, j = n % 9;
-
-
-            if (pu[i][j] != 0)
-
-            {
-                //如果当前格子不需要填数字，就跳到下一个格子
-
-                GetAnswer(n + 1, pu);
-
-                return;
-            }
-
-
-            for (int k = 0; k < 9; k++)
-
-            {
-                var value = k + 1;
-
-
-                pu[i][j] = value; //当前格子进行尝试所有解
-
-                if (IsValid(i, j, pu))
-
-                    GetAnswer(n + 1, pu); //验证通过，就继续下一个
-            }
-
-
-            pu[i][j] = 0; //如果上面的单元无解，还原，就回溯
-
-            return;
-        }
     }
 }
